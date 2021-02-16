a7f2fa59a8638600e1d41e84ac56823ea7228594
Tanguygab <tanguy@gabril.com>
2021-02-16 18:17:22 +0100

    fixed tablistnamesradius

--- src/main/java/io/github/tanguygab/tabadditions/spigot/TABAdditionsSpigot.java
+++ src/main/java/io/github/tanguygab/tabadditions/spigot/TABAdditionsSpigot.java
@@ -5,19 +5,18 @@ import io.github.tanguygab.tabadditions.shared.features.commands.*;
 import io.github.tanguygab.tabadditions.shared.features.rfps.RFP;
 import io.github.tanguygab.tabadditions.shared.features.rfps.RFPManager;
 import io.github.tanguygab.tabadditions.spigot.Features.BukkitEvents;
-import me.neznamy.tab.api.TabPlayer;
-
-import me.neznamy.tab.shared.TAB;
 import org.bukkit.Bukkit;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 import org.bukkit.event.HandlerList;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
+import java.lang.reflect.Method;
 import java.util.*;
 
 public class TABAdditionsSpigot extends JavaPlugin implements CommandExecutor, TabCompleter {
@@ -46,71 +45,71 @@ public class TABAdditionsSpigot extends JavaPlugin implements CommandExecutor, T
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (!(sender instanceof Player)) return true;
-        TabPlayer p = TAB.getInstance().getPlayer(sender.getName());
+        String name = "~Console~";
+        TABAdditions instance = TABAdditions.getInstance();
+        if (sender instanceof Player) name = sender.getName();
+
         if (args.length < 1 || args[0].equalsIgnoreCase("help"))
-            new HelpCmd(p, this.getDescription().getVersion());
-        else
-            switch (args[0].toLowerCase()) {
-                case "reload": {
-                    reload();
-                    p.sendMessage("&aConfig reloaded!",true);
-                    break;
-                }
-                case "actionbar": {
-                    if (!TABAdditions.getInstance().getConfig("").getBoolean("features.actionbars"))
-                        p.sendMessage("&cActionbar feature is not enabled, therefore this command cannot be used",true);
-                    else if (args.length < 2)
-                        p.sendMessage("&cYou have to provide an actionbar!",true);
-                    else {
-                        Map<String,String> section = TABAdditions.getInstance().actionbarConfig.getConfigurationSection("bars.");
-                        if (!section.containsKey(args[1]))
-                            p.sendMessage("&cThis actionbar doesn't exist!",true);
-                        else
-                            new ActionBarCmd(p, args, section.get(args[1]));
-                    }
-                    break;
-                }
-                case "title": {
-                    if (!TABAdditions.getInstance().getConfig("").getBoolean("features.titles"))
-                        p.sendMessage("&cTitle feature is not enabled, therefore this command cannot be used",true);
-                    else if (args.length < 2)
-                        p.sendMessage("&cYou have to provide a title!",true);
-                    else {
-                        Map<String,String> titleSection = TABAdditions.getInstance().titleConfig.getConfigurationSection("titles."+args[1]);
-                        if (titleSection.isEmpty()) {
-                            p.sendMessage("&cThis title doesn't exist!",true);
-                        }
-                        else {
-                            List<Object> titleProperties = new ArrayList<>();
-                            for (Object property : titleSection.keySet())
-                                titleProperties.add(titleSection.get(property));
-                            new TitleCmd(p, args, titleProperties);
-                        }
-                    }
-                    break;
-                }
-                case "fp": {
-                    if (TABAdditions.getInstance().rfpEnabled) {
-                        if (args.length < 2)
-                            p.sendMessage("You have to provide add, remove, edit, info or list.", false);
-                        else if (!args[1].equalsIgnoreCase("list") && args.length < 3)
-                            p.sendMessage("You have to provide a fake player name.", false);
-                        else if (args[1].equalsIgnoreCase("edit") && args.length < 4)
-                            p.sendMessage("You have to provide an action.", false);
-                        else new RealFakePlayerCmd(p, args);
-                    }
-                    break;
-                }
-                case "tags": {
-                    new TagsCmd(p, args);
-                    break;
-                }
-                case "test": {
-                    p.sendMessage("&7Nothing to see here :D",true);
-                    break;
-                }
+            new HelpCmd(name,getDescription().getVersion());
+        else switch (args[0].toLowerCase()) {
+            case "reload": {
+                reload();
+                instance.sendMessage(name, "&aConfig reloaded!");
+                break;
             }
+            case "actionbar": {
+                if (!instance.getConfig("").getBoolean("features.actionbars"))
+                    instance.sendMessage(name, "&cActionbar feature is not enabled, therefore this command cannot be used");
+                else if (args.length < 2)
+                    instance.sendMessage(name, "&cYou have to provide an actionbar!");
+                else {
+                    Map<String, String> section = instance.actionbarConfig.getConfigurationSection("bars.");
+                    if (!section.containsKey(args[1]))
+                        instance.sendMessage(name, "&cThis actionbar doesn't exist!");
+                    else
+                        new ActionBarCmd(name, args, section.get(args[1]));
+                }
+                break;
+            }
+            case "title": {
+                if (!instance.getConfig("").getBoolean("features.titles"))
+                    instance.sendMessage(name,"&cTitle feature is not enabled, therefore this command cannot be used");
+                else if (args.length < 2)
+                    instance.sendMessage(name,"&cYou have to provide a title!");
+                else {
+                    Map<String, String> titleSection = instance.titleConfig.getConfigurationSection("titles." + args[1]);
+                    if (titleSection.isEmpty()) {
+                        instance.sendMessage(name,"&cThis title doesn't exist!");
+                    } else {
+                        List<Object> titleProperties = new ArrayList<>();
+                        for (Object property : titleSection.keySet())
+                            titleProperties.add(titleSection.get(property));
+                        new TitleCmd(name, args, titleProperties);
+                    }
+                }
+                break;
+            }
+            case "fp": {
+                if (instance.rfpEnabled) {
+                    if (args.length < 2)
+                        instance.sendMessage(name,"You have to provide add, remove, edit, info or list.");
+                    else if (!args[1].equalsIgnoreCase("list") && args.length < 3)
+                        instance.sendMessage(name,"You have to provide a fake player name.");
+                    else if (args[1].equalsIgnoreCase("edit") && args.length < 4)
+                        instance.sendMessage(name,"You have to provide an action.");
+                    else new RealFakePlayerCmd(name, args);
+                }
+                break;
+            }
+            case "tags": {
+                new TagsCmd(name, args);
+                break;
+            }
+            case "test": {
+                instance.sendMessage(name,"&7Nothing to see here :D");
+                break;
+            }
+        }
         return true;
     }
 
@@ -137,6 +136,8 @@ public class TABAdditionsSpigot extends JavaPlugin implements CommandExecutor, T
                         List<String> rfpnames = new ArrayList<>();
                         for (RFP rfp : rfps)
                             rfpnames.add(rfp.getConfigName());
+                        if (args[1].equalsIgnoreCase("remove"))
+                            rfpnames.add("_ALL_");
                         return rfpnames;
                     }
                     if (args[1].equalsIgnoreCase("edit") && args.length == 4)
@@ -149,5 +150,24 @@ public class TABAdditionsSpigot extends JavaPlugin implements CommandExecutor, T
             }
         }
         return null;
+    }
+
+    public String itemStack(ItemStack item) {
+        try {
+            Class<?> craftItemStackClazz = ReflectionUtil.getOBCClass("inventory.CraftItemStack");
+            Method asNMSCopyMethod = ReflectionUtil.getMethod(craftItemStackClazz, "asNMSCopy", ItemStack.class);
+            Object nmsItemStackObj = asNMSCopyMethod.invoke(null, item);
+
+            Class<?> nmsItemStackClazz = ReflectionUtil.getNMSClass("ItemStack");
+            Class<?> nbtTagCompoundClazz = ReflectionUtil.getNMSClass("NBTTagCompound");
+            Method saveNmsItemStackMethod = ReflectionUtil.getMethod(nmsItemStackClazz, "save", nbtTagCompoundClazz);
+
+            Object nmsNbtTagCompoundObj = nbtTagCompoundClazz.newInstance();
+            Object itemAsJsonObject = saveNmsItemStackMethod.invoke(nmsItemStackObj, nmsNbtTagCompoundObj);
+
+            return itemAsJsonObject.toString();
+        } catch (Exception e) {
+            throw new Error(e);
+        }
     }
 }
