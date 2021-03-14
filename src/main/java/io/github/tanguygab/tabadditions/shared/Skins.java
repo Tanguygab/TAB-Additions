fb36cfe12765bd2e75982360abd2ce732e3ac8b8
Tanguygab <tanguy@gabril.com>
2021-03-14 12:57:21 +0100

    added texture: skin type and skin config to store textures

--- src/main/java/io/github/tanguygab/tabadditions/shared/Skins.java
+++ src/main/java/io/github/tanguygab/tabadditions/shared/Skins.java
@@ -7,14 +7,14 @@ import me.neznamy.tab.api.TabPlayer;
 import me.neznamy.tab.shared.TAB;
 
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.*;
+import java.nio.charset.StandardCharsets;
 
 public class Skins {
 
     public static Skins instance;
-    public final Map<String,Object> icons = new HashMap<>();
-
 
     public Skins() {
         instance = this;
@@ -25,6 +25,8 @@ public class Skins {
     }
 
     public String[] getPropPlayer(String name) {
+        if (TABAdditions.getInstance().getConfig(ConfigType.SKINS).hasConfigOption("player-head:"+name))
+            return TABAdditions.getInstance().getConfig(ConfigType.SKINS).getStringList("player-head:"+name).toArray(new String[0]);
         try {
             URL url_0 = new URL("https://api.ashcon.app/mojang/v2/user/" + name);
             InputStreamReader reader_0 = new InputStreamReader(url_0.openStream());
@@ -35,13 +37,18 @@ public class Skins {
             JsonObject skin = parser.getAsJsonObject().get("textures").getAsJsonObject().get("raw").getAsJsonObject();
             String value = skin.get("value").getAsString();
             String signature = skin.get("signature").getAsString();
-            return new String[]{value,signature};
+
+            String[] finalskin = new String[]{value,signature};
+            TABAdditions.getInstance().getConfig(ConfigType.SKINS).set("player-head:"+name, finalskin);
+            return finalskin;
         } catch (Exception e) {
             return null;
         }
     }
 
     public String[] getPropSkin(int id) {
+        if (TABAdditions.getInstance().getConfig(ConfigType.SKINS).hasConfigOption("mineskin:"+id))
+            return TABAdditions.getInstance().getConfig(ConfigType.SKINS).getStringList("mineskin:"+id).toArray(new String[0]);
         try {
             URL url_0 = new URL("https://api.mineskin.org/get/id/" + id);
             InputStreamReader reader_0 = new InputStreamReader(url_0.openStream());
@@ -52,7 +59,41 @@ public class Skins {
             String value = skin.get("value").getAsString();
             String signature = skin.get("signature").getAsString();
 
-            return new String[]{value,signature};
+            String[] finalskin = new String[]{value,signature};
+            TABAdditions.getInstance().getConfig(ConfigType.SKINS).set("mineskin:"+id, finalskin);
+            return finalskin;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public String[] generateFromTexture(String texture) {
+        if (TABAdditions.getInstance().getConfig(ConfigType.SKINS).hasConfigOption("texture:"+texture))
+            return TABAdditions.getInstance().getConfig(ConfigType.SKINS).getStringList("texture:"+texture).toArray(new String[0]);
+        try {
+            URL url = new URL("https://api.mineskin.org/generate/url/");
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestProperty("User-Agent", "ExampleApp/v1.0");
+            con.setRequestProperty("Content-Type", "application/json");
+            con.setRequestMethod("POST");
+            con.setDoOutput(true);
+            String jsonInputString = "{\"variant\":\"classic\",\"name\":\"string\",\"visibility\":0,\"url\":\"http://textures.minecraft.net/texture/"+texture+"\"}";
+            try (OutputStream os = con.getOutputStream()) {
+                byte[] input = jsonInputString.getBytes(StandardCharsets.UTF_8);
+                os.write(input, 0, input.length);
+            }
+
+            InputStreamReader reader = new InputStreamReader(con.getInputStream());
+            JsonElement parser = new JsonParser().parse(reader);
+            if (!parser.isJsonObject())
+                return new String[]{"null","null"};
+            JsonObject skin = parser.getAsJsonObject().get("data").getAsJsonObject().get("texture").getAsJsonObject();
+            String value = skin.get("value").getAsString();
+            String signature = skin.get("signature").getAsString();
+
+            String[] finalskin = new String[]{value,signature};
+            TABAdditions.getInstance().getConfig(ConfigType.SKINS).set("texture:"+texture, finalskin);
+            return finalskin;
         } catch (Exception e) {
             return null;
         }
@@ -60,15 +101,12 @@ public class Skins {
 
     public Object getIcon(String icon, TabPlayer p) {
         icon = TABAdditions.getInstance().parsePlaceholders(icon, p);
-        if (icons.containsKey(icon) && icons.get(icon) != null)
-            return icons.get(icon);
-        String deficon = icon;
         Object skin = null;
         String[] props = null;
         if (icon == null || icon.equals("")) return null;
-        if (icon.startsWith("base64:")) {
-            icon = icon.replace("base64:","");
-            props = new String[]{icon,"base64"};
+        if (icon.startsWith("texture:")) {
+            icon = icon.replace("texture:","");
+            props = generateFromTexture(icon);
         }
         if (icon.startsWith("player-head:")) {
             icon = icon.replace("player-head:", "");
@@ -88,7 +126,6 @@ public class Skins {
         if (skin == null && props != null && !props[0].equals("") && !props[1].equals("")) {
             skin = TABAdditions.getInstance().getPlatform().getSkin(props);
         }
-        icons.put(deficon,skin);
         return skin;
     }
 }
