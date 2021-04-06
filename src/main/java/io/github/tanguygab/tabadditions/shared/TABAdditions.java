3e074f93184f78487fd0216a9790f90aaedaeefd
Tanguygab <tanguy@gabril.com>
2021-04-06 12:33:43 +0200

    fixed relational placeholders

--- src/main/java/io/github/tanguygab/tabadditions/shared/TABAdditions.java
+++ src/main/java/io/github/tanguygab/tabadditions/shared/TABAdditions.java
@@ -302,10 +302,22 @@ public class TABAdditions {
 
         List<String> list = TAB.getInstance().getPlaceholderManager().detectAll(str);
         for (String pl : list) {
-            if (pl.startsWith("%sender:") && sender != null)
-                str = str.replace(pl,parsePlaceholders(pl.replace("sender:",""),sender));
-            else if (pl.startsWith("%viewer:") && viewer != null)
-                str = str.replace(pl,parsePlaceholders(pl.replace("viewer:",""),viewer));
+            if (pl.startsWith("%rel_")) {
+                TabPlayer def2 = def == viewer ? sender : viewer;
+                str = str.replace(pl, new Property(def, pl).getFormat(def2));
+            }
+            if (pl.startsWith("%sender:") && sender != null) {
+                String pl2 = pl.replace("%sender:", "");
+                if (pl2.startsWith("%rel_"))
+                    str = str.replace(pl,new Property(sender, pl2).getFormat(viewer));
+                else str = str.replace(pl, parsePlaceholders(pl2, sender));
+            }
+            else if (pl.startsWith("%viewer:") && viewer != null) {
+                String pl2 = pl.replace("%viewer:", "");
+                if (pl2.startsWith("%rel_"))
+                    str = str.replace(pl,new Property(viewer, pl2).getFormat(sender));
+                else str = str.replace(pl, parsePlaceholders(pl2, viewer));
+            }
         }
         str = parsePlaceholders(str,def);
 
