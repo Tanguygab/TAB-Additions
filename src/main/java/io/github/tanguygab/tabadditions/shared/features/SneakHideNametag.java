02b1248c648142088e1e2cb1e18f7614d3c16122
Tanguygab <tanguy@gabril.com>
2021-05-03 19:07:21 +0200

    that got removed

--- src/main/java/io/github/tanguygab/tabadditions/shared/features/SneakHideNametag.java
+++ src/main/java/io/github/tanguygab/tabadditions/shared/features/SneakHideNametag.java
@@ -2,18 +2,36 @@ package io.github.tanguygab.tabadditions.shared.features;
 
 import io.github.tanguygab.tabadditions.shared.TABAdditions;
 import me.neznamy.tab.api.TabPlayer;
-import me.neznamy.tab.shared.cpu.TabFeature;
-import me.neznamy.tab.shared.features.types.event.SneakEventListener;
+import me.neznamy.tab.shared.TAB;
+import me.neznamy.tab.shared.features.types.Loadable;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerToggleSneakEvent;
+import org.bukkit.plugin.Plugin;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class SneakHideNametag implements SneakEventListener {
+public class SneakHideNametag implements Loadable, Listener {
 
     private final Map<TabPlayer, Boolean> tag = new HashMap<>();
 
+    public SneakHideNametag() {
+        load();
+    }
+
     @Override
-    public void onSneak(TabPlayer p, boolean isSneaking) {
+    public void load() {
+        Plugin plugin = (Plugin)TABAdditions.getInstance().getPlugin();
+        plugin.getServer().getPluginManager().registerEvents(this,plugin);
+    }
+
+    @EventHandler
+    public void onSneak(PlayerToggleSneakEvent e) {
+        TabPlayer p = TAB.getInstance().getPlayer(e.getPlayer().getUniqueId());
+        boolean isSneaking = e.isSneaking();
+
         if (TABAdditions.getInstance().sneakhideEnabled) {
             if (isSneaking) {
                 tag.put(p, p.hasHiddenNametag());
@@ -21,6 +39,11 @@ public class SneakHideNametag implements SneakEventListener {
             } else if (!tag.get(p))
                 p.showNametag();
         }
+    }
+
+    @Override
+    public void unload() {
+        HandlerList.unregisterAll(this);
     }
 
     @Override
