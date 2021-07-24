8c9c406171a2a8b53d4a4c4f23acf6e8d59ee00a
Tanguygab <tanguy@gabril.com>
2021-07-24 20:40:23 +0200

    fixed NPE

--- src/main/java/io/github/tanguygab/tabadditions/shared/features/layouts/sorting/Sorting.java
+++ src/main/java/io/github/tanguygab/tabadditions/shared/features/layouts/sorting/Sorting.java
@@ -3,6 +3,7 @@ package io.github.tanguygab.tabadditions.shared.features.layouts.sorting;
 import me.neznamy.tab.api.TabPlayer;
 import me.neznamy.tab.shared.TAB;
 import me.neznamy.tab.shared.cpu.TabFeature;
+import me.neznamy.tab.shared.cpu.UsageType;
 
 import java.util.*;
 
@@ -28,7 +29,7 @@ public class Sorting {
         }
         else types.add(new Groups(""));
 
-        TAB.getInstance().getCPUManager().runTaskLater(1000,"handling TAB+ Layout Sorting", TabFeature.SORTING,null,()->{
+        TAB.getInstance().getCPUManager().runTaskLater(1000,"handling TAB+ Layout Sorting", TabFeature.SORTING, UsageType.REFRESHING,()->{
             List<TabPlayer> players1 = new ArrayList<>(players);
             for (TabPlayer p : players1) {
                 if (!p.isLoaded()) continue;
