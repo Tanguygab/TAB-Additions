2b367f1ee5548bc2a4fdd98773bee74dbf5b9bc1
Tanguygab <tanguy@gabril.com>
2021-09-11 20:27:38 +0200

    I definitely hate my life || added viewer support for tags cmd & */everyone support

--- src/main/java/io/github/tanguygab/tabadditions/shared/commands/TagsCmd.java
+++ src/main/java/io/github/tanguygab/tabadditions/shared/commands/TagsCmd.java
@@ -5,7 +5,11 @@ import me.neznamy.tab.api.TabPlayer;
 import me.neznamy.tab.shared.TAB;
 
 public class TagsCmd {
+
+    private final String name;
+
     public TagsCmd(String name, String[] args) {
+        this.name = name;
 
         TABAdditions instance = TABAdditions.getInstance();
 
@@ -18,27 +22,83 @@ public class TagsCmd {
             return;
         }
 
-        TabPlayer p = TAB.getInstance().getPlayer(args[2]);
+        TabPlayer viewer = args.length > 3 ? (args[3].equals("_self_") ? instance.getPlayer(name) : instance.getPlayer(args[3])) : null;
+
+        if (args[2].equals("*")) {
+            TAB.getInstance().getPlayers().forEach(p2 -> toggleTag(p2, args[1], viewer,false));
+            String action = args[1].toLowerCase()
+                    .replace("hide","cHid")
+                    .replace("show","aShow")
+                    .replace("toggle","7Toggling");
+            sendMsg("&"+action+"ing everyone's nametag"+(viewer != null ?" for "+viewer.getName() : ""));
+            return;
+        }
+
+
+        TabPlayer p = instance.getPlayer(args[2]);
 
         if (p == null) {
             instance.sendMessage(name,"&cThis player isn't connected");
             return;
         }
 
-        switch (args[1]) {
+        toggleTag(p,args[1],viewer,true);
+
+    }
+
+    public void sendMsg(String msg) {
+        TABAdditions.getInstance().sendMessage(name,msg);
+    }
+
+    public void toggleTag(TabPlayer p, String action, TabPlayer viewer, boolean sendmsg) {
+        switch (action) {
             case "show": {
-                p.showNametag();
-                break;
+                if (viewer != null) {
+                    p.showNametag(viewer.getUniqueId());
+                    if (sendmsg) sendMsg("&aShowing "+p.getName()+"'s nametag for "+viewer.getName());
+                }
+                else {
+                    p.showNametag();
+                    if (sendmsg) sendMsg("&aShowing "+p.getName()+"'s nametag for everyone");
+                }
+
+                return;
             }
             case "hide": {
-                p.hideNametag();
-                break;
+                if (viewer != null) {
+                    p.hideNametag(viewer.getUniqueId());
+                    if (sendmsg) sendMsg("&cHiding "+p.getName()+"'s nametag for "+viewer.getName());
+                }
+                else {
+                    p.hideNametag();
+                    if (sendmsg) sendMsg("&cHiding "+p.getName()+"'s nametag for everyone");
+                }
+                return;
             }
             case "toggle": {
-                if (p.hasHiddenNametag()) p.showNametag();
-                else p.hideNametag();
-                break;
+                if (viewer != null) {
+                    if (p.hasHiddenNametag(viewer.getUniqueId())) {
+                        p.showNametag(viewer.getUniqueId());
+                        if (sendmsg) sendMsg("&aToggling on "+p.getName()+"'s nametag for "+viewer.getName());
+                    }
+                    else {
+                        p.hideNametag(viewer.getUniqueId());
+                        if (sendmsg) sendMsg("&cToggling off "+p.getName()+"'s nametag for "+viewer.getName());
+                    }
+                    return;
+                }
+
+                if (p.hasHiddenNametag()) {
+                    p.showNametag();
+                    if (sendmsg) sendMsg("&aToggling on "+p.getName()+"'s nametag for everyone");
+                }
+                else {
+                    p.hideNametag();
+                    if (sendmsg) sendMsg("&cToggling off "+p.getName()+"'s nametag for everyone");
+                }
             }
         }
     }
+
+
 }
